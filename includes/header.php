861075a1aec13668d8f589107b0c22c67e05a933
Mike <parabuzzle@gmail.com>
2009-08-23 11:49:22 -0700

    Call new gif

--- includes/header.php
+++ includes/header.php
@@ -1,5 +1,5 @@
 <div id="header"> 
-		<a href="http://github.com/toobs/toobs"><img style="position: absolute; top: 0; left: 0; border: 0;" src="/images/fork.jpg" alt="Fork me on GitHub" /></a>
+		<a href="http://github.com/toobs/toobs"><img style="position: absolute; top: 0; left: 0; border: 0;" src="/images/forkme.gif" alt="Fork me on GitHub" /></a>
 	<div id="topnav">
 		<a href="http://toobsframework.org">Home</a> | 
 		<a href="http://wiki.toobsframework.org/index.php/docs">Documentation</a> | 
