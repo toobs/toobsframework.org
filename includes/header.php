a2e93efc395a68adbb49584ed3615aa4ba749df1
Mike <parabuzzle@gmail.com>
2009-08-24 10:44:44 -0700

    Changed the header to relative links

--- includes/header.php
+++ includes/header.php
@@ -1,10 +1,10 @@
 <div id="header"> 
 		<a href="http://github.com/toobs/toobs"><img style="position: absolute; top: 0; left: 0; border: 0;" src="/images/forkme.gif" alt="Fork me on GitHub" /></a>
 	<div id="topnav">
-		<a href="http://toobsframework.org">Home</a> | 
+		<a href="/">Home</a> | 
 		<a href="http://wiki.toobsframework.org/index.php/docs">Documentation</a> | 
-		<a href="http://toobsframework.org/community.php">Community</a> | 
-		<a href="http://toobsframework.org/development.php">Development</a> | 
+		<a href="/community.php">Community</a> | 
+		<a href="/development.php">Development</a> | 
 		<a href="http://wiki.toobsframework.org">Wiki</a> | 
 		<a href="http://blog.toobsframework.org">Blog</a>
 	</div>
